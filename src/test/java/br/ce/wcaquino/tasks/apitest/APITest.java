3ea4a882e0cdf57a88531cb5ef0e87b228e3ea05
Ana <anjosanapaula@outlook.com>
2020-07-27 11:51:35 -0300

    Atualizando tarefas.

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -11,7 +11,7 @@ public class APITest {
 	
 	@BeforeClass
 	public static void setup() {
-		RestAssured.baseURI = "http://192.168.15.41:8001/tasks-backend";
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
 	}
 
 	@Test
@@ -29,10 +29,9 @@ public class APITest {
 		RestAssured.given()
 			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
 			.contentType(ContentType.JSON)
-		.when()	
+		.when()
 			.post("/todo")
 		.then()
-			.log().all()
 			.statusCode(201)
 		;
 	}
@@ -42,37 +41,11 @@ public class APITest {
 		RestAssured.given()
 			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
 			.contentType(ContentType.JSON)
-		.when()	
+		.when()
 			.post("/todo")
 		.then()
-			.log().all()
 			.statusCode(400)
 			.body("message", CoreMatchers.is("Due date must not be in past"))
-		;
-	}
-	
-	@Test
-	public void  deveRemoverTarefaComSucesso() {
-		//inserir
-		Integer id = RestAssured.given()
-			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-//			.log().all()
-			.statusCode(201)
-			.extract().path("id")
-		;
-		
-		System.out.println(id);
-		
-		//remover
-		RestAssured.given()
-		.when()
-			.delete("/todo/"+id)
-		.then()
-			.statusCode(204)
 		;
 	}
 }
